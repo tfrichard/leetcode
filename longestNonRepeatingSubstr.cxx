e4b37224af6ecf510ffa3848402b5971300fb03e
feiteng <tf.richard@gmail.com>
2013-08-22 21:31:40 -0400

    two versions

---
+++ longestNonRepeatingSubstr.cxx
@@ -0,0 +1,83 @@
+int n = strlen(str);
+int cur_len = 1;  // To store the lenght of current substring
+int max_len = 1;  // To store the result
+int prev_index;  // To store the previous index
+int i;
+int *visited = (int *)malloc(sizeof(int)*NO_OF_CHARS);
+ 
+/* Initialize the visited array as -1, -1 is used to indicate that
+   character has not been visited yet. */
+for (i = 0; i < NO_OF_CHARS;  i++)
+  visited[i] = -1;
+ 
+/* Mark first character as visited by storing the index of first 
+   character in visited array. */
+visited[str[0]] = 0;
+ 
+/* Start from the second character. First character is already processed
+   (cur_len and max_len are initialized as 1, and visited[str[0]] is set */
+for (i = 1; i < n; i++)
+  {
+    prev_index =  visited[str[i]];
+ 
+    /* If the currentt character is not present in the already processed
+       substring or it is not part of the current NRCS, then do cur_len++ */
+    if (prev_index == -1 || i - cur_len > prev_index)
+      cur_len++;
+ 
+    /* If the current character is present in currently considered NRCS,
+       then update NRCS to start from the next character of previous instance. */
+    else
+      {
+	/* Also, when we are changing the NRCS, we should also check whether 
+	   length of the previous NRCS was greater than max_len or not.*/
+	if (cur_len > max_len)
+	  max_len = cur_len;
+ 
+	cur_len = i - prev_index;
+      }
+ 
+    visited[str[i]] = i; // update the index of current character
+  }
+ 
+// Compare the length of last NRCS with max_len and update max_len if needed
+if (cur_len > max_len)
+  max_len = cur_len;
+ 
+ 
+free(visited); // free memory allocated for visited
+ 
+return max_len;
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+    int hashIndex[256];
+    for (int i = 0; i < 256; i++) hashIndex[i] = -1;
+        
+    int n = s.size();
+    if (n == 0) return 0;
+    int begin = 0;
+    int i = begin;
+    int max = 0;
+    int maxi = 0;
+        
+    while (i < n) {
+      if (hashIndex[s[i]] == -1) {
+	hashIndex[s[i]] = i;
+	i++;
+	if (i - begin > max) max = i - begin;
+      } else {
+	begin = hashIndex[s[i]] + 1 > begin ? hashIndex[s[i]] + 1: begin;
+	//begin = hashIndex[s[i]] + 1;
+	hashIndex[s[i]] = i;
+	i++;
+	if (i - begin > max) max = i - begin;
+      }
+    }
+        
+    return max;
+  }
+};
