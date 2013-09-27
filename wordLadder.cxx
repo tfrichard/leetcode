812a8f6f6d289ac85eb39ef41b016db96d93e4f9
feiteng <tf.richard@gmail.com>
2013-09-27 12:10:26 -0400

    dfs version

--- wordLadder.cxx
+++ wordLadder.cxx
@@ -1,50 +1,56 @@
 class Solution {
 public:
-    int ladderLength(string start, string end, unordered_set<string> &dict) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int depth = dict.size();
-        int size = start.size();
-        int i;
-        map<string, bool> visited;
-        for (auto it = dict.begin(); it != dict.end(); it++) {
-            visited[*it] = false;
-        }
-        visited[start] = true;
+  int ladderLength(string start, string end, unordered_set<string> &dict) {
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+    unordered_map<string, bool> visited;
+    for (string str : dict) visited[str] = false;
         
-        if (isSim(start, end, size)) return 2;
+    int cutoff = 2;
+    if (is_convertable(start, end)) return cutoff;
         
-        for (i = 1; i < depth; i++) {
-            if (dfs(start, end, dict, 0, i, size, visited)) {
-                return i + 2;
-            }
-        }
-        return 0;
+    while (cutoff <= dict.size() + 2) {
+      cutoff += 1;
+      if (dfs(start, end, visited, dict, 1, cutoff)) return cutoff;
+            
     }
+        
+    return 0;
+  }
     
-    bool dfs(string start, string end, unordered_set<string> &dict, int depth, int &limit, int &size, map<string, bool> &visited) {
-        if (depth >= limit) return false;
+  bool dfs(string &start, string &end, unordered_map<string, bool> &visited, 
+	   unordered_set<string> &dict, int depth, const int &cutoff) {
+    if (depth >= cutoff) return false;
         
-        bool res = false;
-        for (auto it = dict.begin(); it != dict.end(); it++) {
-            if (!visited[*it] && isSim(start, *it, size)) {
-                if (isSim(*it, end, size)) return true;
-                else {
-                    visited[*it] = true;
-                    res = dfs(*it, end, dict, depth + 1, limit, size, visited);
-                    visited[*it] = false;
-                }
-            }
-        }
-        return res;
+    if (is_convertable(start, end)) return true;
+        
+    for (string str : dict) {
+      if (visited[str] == true) continue;
+            
+      if (is_convertable(start, str)) {
+	visited[str] = true;
+	if (dfs(str, end, visited, dict, depth + 1, cutoff)) {
+	  return true;
+	}
+	visited[str] = false;
+      }
     }
+        
+    return false;
+  }
     
-    bool isSim(const string &str1, const string &str2, int size) {
-        int numDiff = 0;
-        for (int i = 0; i < size; i++) {
-            if (str1[i] != str2[i]) numDiff++;
-        }
-        if (numDiff == 1) return true;
-        else return false;
+  bool is_convertable(string &src, string &dst) {
+    if (src == dst) return true;
+        
+    int cnt = 0;
+    for (int i = 0; i < src.size(); i++) {
+            
+      if (src[i] != dst[i]) {
+	cnt += 1;
+	if (cnt > 1) return false;
+      }
     }
+        
+    return cnt == 1;
+  }
 };
